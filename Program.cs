298d8d154a81c56c764f799708e35bf6c8258362
Nadusha <nadusha230275@gmail.com>
2022-07-07 22:35:09 +0500

    работает

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// подсчитать сумму цифр в числе
+Console.WriteLine("введите число ");
+int a = int.Parse(Console.ReadLine());
+
+
+int s=0;
+while(a>0)
+{
+    s=s+a%10;
+    a=a/10;
+    
+}
+Console.WriteLine($" сумма цифр числа = {s} ");
